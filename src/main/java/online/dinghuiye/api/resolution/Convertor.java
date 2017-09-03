d97110d6ee9cae17c3528bebf2240e50f42769e4
strangeen <strangeen@aliyun.com>
2017-09-04 00:52:47 +0800

    注释修正

--- src/main/java/online/dinghuiye/api/resolution/Convertor.java
+++ src/main/java/online/dinghuiye/api/resolution/Convertor.java
@@ -16,10 +16,10 @@ import java.util.Map;
 public interface Convertor {
 
     /**
-     * 将excel元数据转换为对应pojo属性类型的值
-     * @param obj 需要转换的值，如果是excel元数据则为String
+     * 将excel数据转换为对应pojo属性类型的值
+     * @param obj 需要转换的值，根据excel单元格格式转换为对应的对象格式
      * @param field pojo属性字段
-     * @param excelRecordMap excel元数据map<表头名称, 单元格值>（预留参数）
+     * @param excelRecordMap excel行数据map<表头名称, 单元格值>（预留参数）
      * @return 转换后的值
      */
     Object convert(Object obj, Field field, Map<String, Object> excelRecordMap);
